b8c29e1b8891ac458af2b37856a160240c2eba2f
Trung Hieu <hieutt@d-soft.com.vn>
2020-03-06 13:53:11 +0700

    Fix wanrning navigationOption

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -34,7 +34,7 @@ const IndexScreen = ({ navigation }) => {
 
 IndexScreen.navigationOptions = ({ navigation }) => {
 	return {
-		headerRight: (
+		headerRight: () => (
 			<TouchableOpacity onPress={() => navigation.navigate('Create')}>
 				<Feather name="plus" size={30} />
 			</TouchableOpacity>
